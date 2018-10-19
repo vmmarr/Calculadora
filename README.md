903b333f52f5359757dd071b95e18ac15d8fbf9c
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2018-10-19 20:52:02 +0200

    Carga inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculadora
+
+Tiene que tener un formulario con 3 input de tipo text.
+  Primer operando
+  Segundo operando
+  Operacion
+  Boton Calcular.
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Calculadora</title>
+    </head>
+    <body>
+        <form action="" method="get">
+            <label>Primer operando</label><input type="text" name="" value="">
+            <label>Segundo operando</label><input type="text" name="" value="">
+            <label>Operacion</label><input type="text" name="" value="">
+            <input type="button" value="Calcular">
+        </form>
+    </body>
+</html>
